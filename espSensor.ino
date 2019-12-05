f9df954003a8a9a32c70e5511336de29ec54677e
JaakkoK <jaska91@hotmail.com>
2019-12-05 15:26:18 +0200

    initial commit

---
+++ espSensor.ino
@@ -0,0 +1,36 @@
+#include <sSense-CCS811.h>
+#include <Adafruit_BME280.h>
+
+
+Adafruit_BME280 bme;
+
+
+void setup() {
+	Serial.begin(9600);
+
+	if (!bme.begin(0x76)) {
+		Serial.println("Could not find a valid BME280 sensor, check wiring!");
+		while (1);
+	}
+}
+
+void loop() {
+	Serial.print("Temperature = ");
+	Serial.print(bme.readTemperature());
+	Serial.println("*C");
+
+	Serial.print("Pressure = ");
+	Serial.print(bme.readPressure() / 100.0F);
+	Serial.println("hPa");
+
+	Serial.print("Approx. Altitude = ");
+	Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+	Serial.println("m");
+
+	Serial.print("Humidity = ");
+	Serial.print(bme.readHumidity());
+	Serial.println("%");
+
+	Serial.println();
+	delay(1000);
+}
